1174509d0b0011d3cdf4b0dc8cdee0224e0f3e7d
ajitkumar-tech <ajitsanjeevi229@gmail.com>
2026-04-16 13:32:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -129,7 +129,7 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
 resource "aws_eks_cluster" "eks_cluster" {
   name     = "my-eks-cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn 
-  version = "1.28"
+  version = "1.29"
 
   vpc_config {
     subnet_ids         = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
